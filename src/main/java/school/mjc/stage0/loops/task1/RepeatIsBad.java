02db8475cc3078f1a5f3d3b98c23c4d5bfc18879
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-07-30 19:59:38 +0300

    All tasks completed.

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,33 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        String repeatedSentence = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+        System.out.println(repeatedSentence);
+
+
+
+
+
+
 
     }
 }
